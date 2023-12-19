570e0776803f9d6d9afbb90ffa4c3f3e7daa5be6
Lucky_World <128409984+Nirmalkumar882000@users.noreply.github.com>
2023-12-19 11:39:44 +0530

    project setup  completed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <h1 className=" flex justify-center items-center  text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  )
+}
+
+export default App
